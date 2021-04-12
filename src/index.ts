10b47941814e411783b189ff415561b08c7223ad
colter <89688563@qq.com>
2021-04-12 15:22:54 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
-function f() {
-  console.log("f(): evaluated");
-  return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
-    console.log("f(): called");
-  }
-}
-
-interface IG {
-  method: 'POST' | 'GET' | 'PUT' | 'DELETE';
-}
-
-function g(param: IG) {
-  console.log("g(): evaluated", param);
-  return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
-    console.log("g(): called", target, propertyKey, descriptor);
-  }
-}
-
-class C {
-  @f()
-  @g({ method: 'POST' })
-  method() {}
-}
-
-new C();
-console.log('--->', C);
+export * from './interface';
+export * from './decorator';
+export * from './register';
